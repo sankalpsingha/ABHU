80cb4bab13fda3be6b792f241babfaa1ce2d002d
Sankalp Singha <sankalp@srlabs.id>
2021-07-29 19:50:43 +0700

    Added the navigation and other stuffs

--- App.js
+++ App.js
@@ -1,54 +1,31 @@
-// import { StatusBar } from 'expo-status-bar';
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Sankalp yaya!!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-
-
-
 import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import {View} from 'react-native';
+import Login from './src/pages/Login';
+import Home from './src/pages/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyle}>Hello World!!</Text>
-      <TouchableOpacity>
-        <Text>Hello button</Text>
-      </TouchableOpacity>
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen
+                  options={{ headerShown: false }}
+                  name="Login"
+                  component={Login}
+              />
+              <Stack.Screen name="Home" component={Home} />
+          </Stack.Navigator>
+      </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
+const style = {
   container: {
-    backgroundColor: 'tomato',
     flex: 1,
-    padding: 10,    
-  },
-  textStyle: {
-    textAlign: 'center',
-    color: 'green',
-    borderRadius: 1
   }
-
-})
+};
 
 export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,72 @@
+import React, {useState} from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, Image, TextInput } from 'react-native';
+import logo from '../../assets/logo.png';
+
+const Login = ({navigation}) => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    return (
+        <View style={styles.container}>
+            <Image source={logo} style={styles.imageStyle} />
+            <TextInput
+                onChangeText={setEmail}
+                value={email}
+                placeholder="Enter Email"
+                style={styles.loginField}
+            />
+            <TextInput
+                onChangeText={setPassword}
+                value={password}
+                placeholder="Enter Password"
+                style={styles.loginField}
+                secureTextEntry={true}
+            />
+            <TouchableOpacity 
+              style={styles.button}
+              onPress= {()=> navigation.navigate('Home')}
+            >
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#2F2F2F',
+        flex: 1,
+        padding: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textStyle: {
+        textAlign: 'center',
+    },
+    imageStyle: {
+        width: 220,
+        height: 130,
+        marginTop: -100,
+    },
+    loginField: {
+        height: 40,
+        padding: 15,
+        backgroundColor: '#EAEAEA',
+        marginTop: 10,
+        width: '100%',
+        borderRadius: 3,
+    },
+    button: {
+        padding: 13,
+        backgroundColor: '#E63359',
+        marginTop: 10,
+        borderRadius: 3,
+        alignSelf: 'stretch'
+    },
+    buttonText: {
+        color: 'white',
+        textTransform: 'uppercase',
+        textAlign: 'center'
+    }
+});
+
+export default Login;
